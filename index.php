9cc4ded71800432a921c6fcd19032e293ad86163
fequiso <1006320181@unajma.edu.pe>
2020-08-17 15:42:43 -0500

    login con css

--- index.php
+++ index.php
@@ -3,14 +3,20 @@
   <head>
     <meta charset="utf-8">
     <title>ARTEFACTOS ELECTRÓNICOS</title>
+    <link rel="stylesheet" href="css/estilo.css">
   </head>
   <body>
-    <form class="" action="index.html" method="post">
-      <h3>iniciar sesión</h3>
-      <img src="imagenes/user.png" alt="login">
-      <input type="text" name="usuario" placeholder="usuario" value="">
-      <input type="password" name="clave" placeholder="contraseña" value="">
-      <input type="submit" name="" value="INGRESAR">
-    </form>
+    <section id="contenedor">
+      <form class="" action="" method="post">
+        <h3>INICIAR SESIÓN</h3>
+        <img src="imagenes/user.png" width="200" height="200" alt="login">
+        <input type="text" name="usuario" placeholder="usuario" value="">
+        <input type="password" name="clave" placeholder="contraseña" value="">
+        <input type="submit" name="" value="INGRESAR">
+        <form  action="registrate.php" method="post">
+          <input   type="submit" name="" value="REGISTRARSE">
+        </form>
+      </form>
+    </section>
   </body>
 </html>
