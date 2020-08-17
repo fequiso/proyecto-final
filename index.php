c0d9a05b254d8b03094f4c81f4b9c6d9ded25444
fequiso <1006320181@unajma.edu.pe>
2020-08-17 15:04:59 -0500

    login

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ARTEFACTOS ELECTRÓNICOS</title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <h3>iniciar sesión</h3>
+      <img src="imagenes/user.png" alt="login">
+      <input type="text" name="usuario" placeholder="usuario" value="">
+      <input type="password" name="clave" placeholder="contraseña" value="">
+      <input type="submit" name="" value="INGRESAR">
+    </form>
+  </body>
+</html>
