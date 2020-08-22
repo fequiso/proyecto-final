817edbb02f1ccdc883ab5716d58d355e15be6f7a
fequiso <1006320181@unajma.edu.pe>
2020-08-22 16:54:15 -0500

    menus

---
+++ gaaa/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo1.css">
+	<title>ELECTRONICA | FEQUISO</title>
+</head>
+<body>
+	<header>
+		<div class="header">
+
+			<h2>ELECTRÓNICA FEQUISO <h3>venta telefónica 962249355</h3> </h2>
+
+			<div class="optionsBar">
+				<p>PERÚ-APURIMAC-ANDAHUAYLAS</p>
+				<span>|</span>
+				<span class="user"></span>
+				<img class="photouser" src="img/user.jpg" alt="Usuario">
+				<a href="salir.php"><img class="close" src="img/pp.png" alt="Salir del sistema" title="Salir"></a>
+			</div>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+
+				<li class="principal">
+					<a href="#">Usuarios</a>
+					<ul>
+						<li><a href="#">Nuevo Usuario</a></li>
+						<li><a href="#">Lista de Usuarios</a></li>
+					</ul>
+				</li>
+
+				<li class="principal">
+					<a href="#">Clientes</a>
+					<ul>
+						<li><a href="#">Nuevo Cliente</a></li>
+						<li><a href="#">Lista de Clientes</a></li>
+					</ul>
+				</li>
+
+				<li class="principal">
+					<a href="#">Proveedores</a>
+					<ul>
+						<li><a href="#">Nuevo Proveedor</a></li>
+						<li><a href="#">Lista de Proveedores</a></li>
+					</ul>
+				</li>
+
+				<li class="principal">
+					<a href="#">Productos</a>
+					<ul>
+						<li><a href="#">Nuevo Producto</a></li>
+						<li><a href="#">Lista de Productos</a></li>
+					</ul>
+				</li>
+
+				<li class="principal">
+					<a href="#">Facturas</a>
+					<ul>
+						<li><a href="#">Nuevo Factura</a></li>
+						<li><a href="#">Facturas</a></li>
+					</ul>
+				</li>
+
+			</ul>
+		</nav>
+	</header>
+	<section id="container">
+		<h1></h1>
+	</section>
+</body>
+</html>
