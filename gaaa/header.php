c42ebacf20f8140f1c6e866dc86d5a86056d0759
fequiso <1006320181@unajma.edu.pe>
2020-08-26 18:13:45 -0500

    ventanas

---
+++ gaaa/header.php
@@ -0,0 +1,15 @@
+<header>
+  <div class="header">
+
+    <h2>ELECTRÓNICA FEQUISO <h3>venta telefónica 962249355</h3> </h2>
+
+    <div class="optionsBar">
+      <p>PERÚ-APURIMAC-ANDAHUAYLAS</p>
+      <span>|</span>
+      <span class="user"></span>
+      <img class="photouser" src="img/user.jpg" alt="Usuario">
+      <a href="salir.php"><img class="close" src="img/pp.png" alt="Salir del sistema" title="Salir"></a>
+    </div>
+  </div>
+  <?php include 'nav.php'; ?>
+</header>
---
+++ gaaa/scripts.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" type="text/css" href="css/estilo1.css">
+<script type="text/javascript" src="js/functions.js" ></script>
